20ee7ca2291afc1dd8ff2698b8d36d2bb98ee598
Gagan <gagankaushal@users.noreply.github.com>
2020-03-31 02:52:15 -0400

    Add files via upload

--- VoiceAssistant.py
+++ VoiceAssistant.py
@@ -4,7 +4,16 @@ import speech_recognition as sr
 import os
 # import sys
 import re
+import dbus
+import dbus.glib
+import dbus.service
+import gobject
+
+
+from datetime import datetime, timedelta
 import webbrowser
+# from xdo import Xdo
+from selenium import webdriver
 # import smtplib
 # import requests
 # import subprocess
@@ -12,7 +21,7 @@ import webbrowser
 # import youtube_dl
 # import vlc
 # import urllib
-import urllib3
+import urllib.request as urllib2
 # import json
 from bs4 import BeautifulSoup as soup
 # from urllib2 import urlopen
@@ -53,23 +62,29 @@ def assistant(command):
                             sofiaResponse('The website you have requested has been opened for you Gagan.')
                     else:
                             pass
-    elif 'play a song' in command:
+    elif 'play' in command:
         # elif 'play me a song' in command:
+                    sofiaResponse('What song shall I play Gagan?')
                     mysong = myCommand()
         # elif 'play me a song' in command:
             # mysong = myCommand()
-                    if mysong:
-                            flag = 0
-                            url = "https://www.youtube.com/results?search_query=" + mysong.replace(' ', '+')
-                            response = urllib3.urlopen(url)
-                            html = response.read()
-                            soup1 = soup(html,"lxml")
-                            url_list = []
-                            for vid in soup1.findAll(attrs={'class':'yt-uix-tile-link'}):
-                                    if ('https://www.youtube.com' + vid['href']).startswith("https://www.youtube.com/watch?v="):
-                                            flag = 1
-                                            final_url = 'https://www.youtube.com' + vid['href']
-                                            url_list.append(final_url)
+                    # if mysong:
+                    flag = 0
+                    url = "https://www.youtube.com/results?search_query=" + mysong.replace(' ', '+')
+                    response = urllib2.urlopen(url)
+                    html = response.read()
+                    soup1 = soup(html,"lxml")
+                    url_list = []
+                    for vid in soup1.findAll(attrs={'class':'yt-uix-tile-link'}):
+                            if ('https://www.youtube.com' + vid['href']).startswith("https://www.youtube.com/watch?v="):
+                                    flag = 1
+                                    final_url = 'https://www.youtube.com' + vid['href']
+                                    url_list.append(final_url)
+                                    webbrowser.open(final_url)
+                                    sofiaResponse('Enjoy the song, Gagan!')
+                                    break
+
+
     # url = url_list[0]
     #             ydl_opts = {}
     # os.chdir(path)
@@ -78,6 +93,93 @@ def assistant(command):
     #             vlc.play(path)
     # if flag == 0:
     #                 sofiaResponse('I have not found anything in Youtube ')
+
+
+def youtubePause():
+
+    # xdo = Xdo()
+    # win_id = xdo.select_window_with_click()
+    # Using Chrome to access web
+    driver = webdriver.Chrome()
+
+# def main():
+#     """Open youtube and start dbus listener."""
+#     global browser
+
+#     # loop = gobject.MainLoop()
+
+#     # bus = dbus.SessionBus()
+#     # bus_name = dbus.service.BusName('org.mpris.MediaPlayer2.youtube', bus=bus)
+
+#     # MediaPlayer2(bus_name, '/org/mpris/MediaPlayer2', loop)
+
+#     browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+#     browser.get('https://youtube.com')
+
+#     # loop.run()
+
+
+# class MediaPlayer2(dbus.service.Object):
+#     # def __init__(self, bus_name, object_path, loop):
+#     #     dbus.service.Object.__init__(self, bus_name, object_path)
+#     #     self.loop = loop
+
+#     @dbus.service.method('org.mpris.MediaPlayer2.Player')
+#     def Previous(self):
+#         global browser, last_previous
+#         print('Received previous signal')
+#         now = datetime.now()
+#         if now - last_previous < timedelta(seconds=5):
+#             browser.back()
+#         else:
+#             elem = browser.find_element_by_tag_name('video')
+#             elem.send_keys('0')
+#         last_previous = now
+
+#     @dbus.service.method('org.mpris.MediaPlayer2.Player')
+#     def PlayPause(self):
+#         global browser
+#         print('Received PlayPause signal')
+#         browser.execute_script(
+#             'document.getElementsByTagName("video")[0].paused ?'
+#             'document.getElementsByTagName("video")[0].play() :'
+#             'document.getElementsByTagName("video")[0].pause();')
+#         #elem = browser.find_element_by_class_name('ytp-play-button')
+#         #elem.click()
+
+#     @dbus.service.method('org.mpris.MediaPlayer2.Player')
+#     def Next(self):
+#         global browser
+#         print('Received next signal')
+#         next_video_url = _find_next_video_url(browser)
+#         if next_video_url:
+#             browser.get(next_video_url)
+#         else:
+#             browser.forward()
+
+
+# def _find_next_video_url(c):
+#     """Try different ways to determine the url of the next video."""
+#     try:
+#         next_video_url = sorted(Counter(
+#             a.get_property('href')
+#             for a in c.find_elements_by_tag_name('a')
+#             if a.get_property('href').find('/watch?') >= 0
+#         ).items(), key=lambda k: k[1], reverse=True)[0][0]
+#     except Exception:
+#         pass
+#     if next_video_url:
+#         return next_video_url
+#     next_video_url = browser.find_element_by_class_name('watch-sidebar-body') \
+#         .find_element_by_tag_name('a').get_property('href')
+#     if next_video_url:
+#         return next_video_url
+
+# browser = None
+# last_previous = datetime.now()
+
+# if __name__ == '__main__':
+#     main()
 
 num = 1
 
@@ -99,7 +201,9 @@ def sofiaResponse(output):
         playsound.playsound(file, True)  
         os.remove(file)
 
+sofiaResponse('Hi Gagan, this is Aisha and I am your personal voice assistant. How can I help you today?')# Please give a command or say "help me" and I will tell you what all I can do for you.'how can I help you today?')
+
+
 while True:
-    sofiaResponse('Hi Gagan, how can I help you today?')
     assistant(myCommand())
 # assistant(myCommand())
