5392d94458a0e8158cd521ed5b84f52c9dadd15b
Gagan <gagankaushal@users.noreply.github.com>
2020-06-01 22:01:33 -0400

    Updated "assistant" function with 'play' command

--- VoiceAssistant.py
+++ VoiceAssistant.py
@@ -2,32 +2,12 @@ from gtts import gTTS
 import playsound
 import speech_recognition as sr
 import os
-# import sys
 import re
-# import dbus
-# import dbus.glib
-# import dbus.service
-# import gobject
-
-
 from datetime import datetime, timedelta
 import webbrowser
-# from xdo import Xdo
 from selenium import webdriver
-# import smtplib
-# import requests
-# import subprocess
-# from pyowm import OWM
-# import youtube_dl
-# import vlc
-# import urllib
 import urllib.request as urllib2
-# import json
 from bs4 import BeautifulSoup as soup
-# from urllib2 import urlopen
-# import wikipedia
-# import random
-# from time import strftime
 
 def myCommand():
         r = sr.Recognizer()
@@ -45,11 +25,6 @@ def myCommand():
                 command = myCommand();
         return command
 
-
-# def sofiaResponse(audio):
-#     print(audio)
-#     for line in audio.splitlines():
-#         os.system("say " + audio)
 
 def assistant(command):
     if 'open' in command:
@@ -105,16 +80,6 @@ def assistant(command):
         sofiaResponse('Video has been resumed!')
 
 
-    # url = url_list[0]
-    #             ydl_opts = {}
-    # os.chdir(path)
-    #             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #                 ydl.download([url])
-    #             vlc.play(path)
-    # if flag == 0:
-    #                 sofiaResponse('I have not found anything in Youtube ')
-
-
 def youtubePause():
 
     # xdo = Xdo()
@@ -122,84 +87,6 @@ def youtubePause():
     # Using Chrome to access web
     driver = webdriver.Chrome()
 
-# def main():
-#     """Open youtube and start dbus listener."""
-#     global browser
-
-#     # loop = gobject.MainLoop()
-
-#     # bus = dbus.SessionBus()
-#     # bus_name = dbus.service.BusName('org.mpris.MediaPlayer2.youtube', bus=bus)
-
-#     # MediaPlayer2(bus_name, '/org/mpris/MediaPlayer2', loop)
-
-#     browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-#     browser.get('https://youtube.com')
-
-#     # loop.run()
-
-
-# class MediaPlayer2(dbus.service.Object):
-#     # def __init__(self, bus_name, object_path, loop):
-#     #     dbus.service.Object.__init__(self, bus_name, object_path)
-#     #     self.loop = loop
-
-#     @dbus.service.method('org.mpris.MediaPlayer2.Player')
-#     def Previous(self):
-#         global browser, last_previous
-#         print('Received previous signal')
-#         now = datetime.now()
-#         if now - last_previous < timedelta(seconds=5):
-#             browser.back()
-#         else:
-#             elem = browser.find_element_by_tag_name('video')
-#             elem.send_keys('0')
-#         last_previous = now
-
-#     @dbus.service.method('org.mpris.MediaPlayer2.Player')
-#     def PlayPause(self):
-#         global browser
-#         print('Received PlayPause signal')
-#         browser.execute_script(
-#             'document.getElementsByTagName("video")[0].paused ?'
-#             'document.getElementsByTagName("video")[0].play() :'
-#             'document.getElementsByTagName("video")[0].pause();')
-#         #elem = browser.find_element_by_class_name('ytp-play-button')
-#         #elem.click()
-
-#     @dbus.service.method('org.mpris.MediaPlayer2.Player')
-#     def Next(self):
-#         global browser
-#         print('Received next signal')
-#         next_video_url = _find_next_video_url(browser)
-#         if next_video_url:
-#             browser.get(next_video_url)
-#         else:
-#             browser.forward()
-
-
-# def _find_next_video_url(c):
-#     """Try different ways to determine the url of the next video."""
-#     try:
-#         next_video_url = sorted(Counter(
-#             a.get_property('href')
-#             for a in c.find_elements_by_tag_name('a')
-#             if a.get_property('href').find('/watch?') >= 0
-#         ).items(), key=lambda k: k[1], reverse=True)[0][0]
-#     except Exception:
-#         pass
-#     if next_video_url:
-#         return next_video_url
-#     next_video_url = browser.find_element_by_class_name('watch-sidebar-body') \
-#         .find_element_by_tag_name('a').get_property('href')
-#     if next_video_url:
-#         return next_video_url
-
-# browser = None
-# last_previous = datetime.now()
-
-# if __name__ == '__main__':
-#     main()
 
 num = 1
 
@@ -227,4 +114,3 @@ driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
 while True:
     assistant(myCommand())
-# assistant(myCommand())
